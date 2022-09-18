e75055a8c317f7304dd1df8dc74c041431fd95f3
proloypaul <proloyp23@gmail.com>
2022-09-18 20:10:29 +0600

    from html

--- src/Component/AddImg/Addimg.js
+++ src/Component/AddImg/Addimg.js
@@ -4,7 +4,23 @@ const Addimg = () => {
     return (
         <div>
             <h1>Add img section</h1>
-            
+            <from>
+                <div>
+                    <input type="file" name="img"/>
+                </div>
+                <div>
+                    <input type="text" name="name" placeholder="Enter your Name"/>
+                </div>
+                <div>
+                    <input type="email" name="email" placeholder="email@gmail.com"/>
+                </div>
+                <div>
+                    <input placeholder="Enter 8 charecter" type="password" name="password" />
+                </div>
+                <div>
+                    <button type="submit">Upload</button>
+                </div>
+            </from>
         </div>
     );
 };
