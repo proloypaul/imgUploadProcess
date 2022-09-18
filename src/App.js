1d1f327fc97ec1c5c6f0afad054c9139908c79fd
proloypaul <proloyp23@gmail.com>
2022-09-18 20:04:43 +0600

    setUp react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Addimg from '../src/Component/AddImg/Addimg';
+function App() {
+  return (
+    <div className="App">
+      <Addimg></Addimg>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/AddImg/Addimg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Addimg = () => {
+    return (
+        <div>
+            <h1>Add img section</h1>
+            
+        </div>
+    );
+};
+
+export default Addimg;
