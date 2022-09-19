47ea15388f55cdd722b170720b1c6bccf3adef2e
proloypaul <proloyp23@gmail.com>
2022-09-19 11:42:44 +0600

    collect data

--- src/Component/AddImg/Addimg.js
+++ src/Component/AddImg/Addimg.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Addimg = () => {
+    const defaultData = {date: "10:05"}
+    const [storeData, setStoreData] = useState(defaultData);
+
+    const collectData = (event) => {
+        const field = event.target.name;
+        const value = event.target.value;
+        let allStoreData = {...storeData};
+        allStoreData[field] = value; 
+        setStoreData(allStoreData);
+        console.log(storeData.image);
+    }
+    const handleData = () => {
+        
+    }
     return (
         <div>
             <h1>Add img section</h1>
-            <from>
+            <form onClick={() => handleData()}>
                 <div>
-                    <input type="file" name="img"/>
+                    <input type="file" name="image" required onBlur={collectData}/>
                 </div>
                 <div>
-                    <input type="text" name="name" placeholder="Enter your Name"/>
+                    <input type="text" name="name" placeholder="Enter your Name" required onBlur={collectData} />
                 </div>
                 <div>
-                    <input type="email" name="email" placeholder="email@gmail.com"/>
+                    <input type="email" name="email" placeholder="email@gmail.com" onBlur={collectData} required/>
                 </div>
                 <div>
-                    <input placeholder="Enter 8 charecter" type="password" name="password" />
+                    <input placeholder="Enter 8 charecter" type="password" name="password" onBlur={collectData} required/>
                 </div>
                 <div>
                     <button type="submit">Upload</button>
                 </div>
-            </from>
+            </form>
         </div>
     );
 };
